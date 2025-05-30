b5bd11a9dd3b3c9c0ba7de2f19b336a4c57c1830
MadushanR <madushanrajendran@gmail.com>
2025-05-29 23:00:55 -0400

    fixed output file

--- variables.tf
+++ variables.tf
@@ -1,9 +1,7 @@
 variable "aws_region" {
-  description = "AWS region to deploy resources in"
-  default     = "us-east-1"
+  default = "us-east-1"
 }
 
 variable "key_name" {
-  description = "AWS EC2 Key Pair name for SSH access"
-  type        = string
+  default = "your-key-name"
 }
